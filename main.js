dea4330aec132be1e99edc78bcf69c2c1b877c75
bobbyPischer <robmelmeth@gmail.com>
2022-01-03 15:29:06 +1000

    completed

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const companies= [
 ];
 
 
+
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 // // for (let i = 0; i < companies.length; i++) {
@@ -26,11 +27,13 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // });
 
 
-
+//---------------------------------------------------------------------------------
 
 
 // filter
 // Get 21 and older
+
+//OLD WAY (ES 5)
 
 // let canDrink = [];
 // for(let i = 0; i < ages.length; i++) {
@@ -38,7 +41,11 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //         canDrink.push(ages[i]);
 //     }
 // }
+
+//BETTER WAY (ES 6)
 // const canDrink = ages.filter(age => age >= 21);
+
+
 
 // Filter retail companies
 
@@ -56,18 +63,109 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 //  console.log(lastedTen);
 
+//-------------------------------------------------------------------------------------------------------------
+
 
 // map
 // // Create array of company names
+
+//OLD WAY (ES 5)
+
 // const companyNames = companies.map(function(company) {
 //     return company.name;
 // });
 // console.log(companyNames)
 
-const testMap = companies.map(company => `${company.name} [${company.start}] [${company.end}]`);
-console.log(testMap)
+//BETTER WAY (ES 6)
 
+// const testMap = companies.map(company => `${company.name} [${company.start}] [${company.end}]`);
+// console.log(testMap)
 
+// USING MULTIPLE MAPS TO MANIPULATE DATA MANY TIMES
+
+// const ageMap = ages
+//     .map(age => Math.sqrt(age))
+//     .map(age => age * 2);
+
+// console.log(ageMap);
+
+//---------------------------------------------------------------------------------
 
 // sort
+
+// sort companies by start year
+
+// OLD WAY (ES 5)
+
+// const sortCompanies = companies.sort(function(c1, c2) {
+//     if(c1.start > c2.start) {
+//         return 1;
+//     } else {
+//         return -1;
+//     }
+// });
+// console.log(sortCompanies);
+
+// BETTER WAY (ES 6) with TERNARY operator instead of if/else
+
+// const sortedCompanies = companies.sort((a, b) => a.start > b.start ? 1 : -1);
+//     console.log(sortedCompanies);
+
+// Sort Ages in Ascending Order
+const sortAgeAscending = ages.sort((a, b) => a - b);
+
+//Sort Ages in Descending Order
+const sortAgeDescending = ages.sort((a, b) => b - a);
+
+
+
 // reduce
+
+//Get Sum of entire Array
+
+//With a For Loop 
+
+// let ageSum = 0;
+// for(let i = 0; i < ages.length; i++) {
+//     ageSum += ages[i];
+// }
+// console.log(ageSum);
+
+// Using REDUCE (Long Way)
+
+// const ageSum2 = ages.reduce(function(total, age) {
+//     return total + age;
+// },0);
+
+// console.log(ageSum2);
+
+// BEST WAY
+
+// const ageSum2 = ages.reduce((total, age) => total + age, 0);
+
+// console.log(ageSum2);
+
+
+// //Total Years For All Companies
+
+// const totalYears = companies.reduce(function(total, company) {
+//     return total + (company.end - company.start);
+// }, 0); 
+
+// // Same using arrow functions
+
+// const totalYears2 = companies.reduce((total, company) => total + company.end - company.start, 0);
+
+
+// console.log(totalYears2);
+
+
+// Combine Methods
+
+const combined = ages
+    .map(age => age * 2)
+    .filter(age => age >= 40)
+    .sort((a, b) => a - b)
+    .reduce((a, b) => a + b, 0);
+
+console.log(combined);
